4c4b8321aecf163db1cbdeef94c6c83e2d2fec17
tyraelHqy <752546894@qq.com>
2021-05-06 22:29:50 +0800

    Initial commit

---
+++ src/main/java/com/tyrael/tyraelcoupon/TyraelCouponApplication.java
@@ -0,0 +1,30 @@
+package com.tyrael.tyraelcoupon;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class TyraelCouponApplication {
+
+    public static void main(String[] args) {
+
+        // 1. 通过静态的run方法，启动Springboot
+		SpringApplication.run(TyraelCouponApplication.class, args);
+
+        // 2. 通过Api调整应用行为
+//		SpringApplication application = new SpringApplication(TyraelCouponApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF);
+//		application.setWebApplicationType(WebApplicationType.NONE);
+//		application.run(args);
+
+        // 3. SpringApplicationBuilder Fluent Api，链式调用
+//        new SpringApplicationBuilder(TyraelCouponApplication.class)
+//                .bannerMode(Banner.Mode.OFF)
+//                .web(WebApplicationType.NONE)
+//                .run(args);
+    }
+
+}
---
+++ src/test/java/com/tyrael/tyraelcoupon/TyraelCouponApplicationTests.java
@@ -0,0 +1,9 @@
+package com.tyrael.tyraelcoupon;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyraelCouponApplicationTests {
+
+}
