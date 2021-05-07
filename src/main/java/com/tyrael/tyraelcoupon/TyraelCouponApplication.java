99b01001420ccfc087c79522e02ccb55c5c0c6aa
tyraelHqy <752546894@qq.com>
2021-05-07 23:59:58 +0800

    1. 定时任务功能实现

--- src/main/java/com/tyrael/tyraelcoupon/TyraelCouponApplication.java
+++ src/main/java/com/tyrael/tyraelcoupon/TyraelCouponApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TyraelCouponApplication {
 
