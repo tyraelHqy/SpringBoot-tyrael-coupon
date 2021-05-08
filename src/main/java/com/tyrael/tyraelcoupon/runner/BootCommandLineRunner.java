e485da0d97c1d86a6421f4c790fffb5e1cbd2b3f
tyraelHqy <752546894@qq.com>
2021-05-08 18:42:11 +0800

    1. 完成开机启动，并且可以通过@Order注解自定义顺序

---
+++ src/main/java/com/tyrael/tyraelcoupon/runner/BootCommandLineRunner.java
@@ -0,0 +1,16 @@
+package com.tyrael.tyraelcoupon.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Slf4j
+@Component
+public class BootCommandLineRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("This is BootCommandLineRunner...");
+    }
+}
